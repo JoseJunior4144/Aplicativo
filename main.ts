b9a9cea0be1bc6a0b10edeecf5e941fa655fe78e
GilvanPC <you@example.com>
2023-08-23 20:59:42 -0300

    update Config Base

---
+++ main.ts
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+    const win = new BrowserWindow ({
+        width: 800,
+        height: 800,
+    });
+    win.loadURL("http://localhost:5173/");
+};
+
+app.whenReady().then(() => {
+    createWindow();
+
+    //Para mac-os
+    app.on("activate", () => {
+        if(BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on("window-all-closed", () => {
+    if(process.platform !== "darwin") app.quit();
+})
